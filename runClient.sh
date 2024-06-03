1ca31540a5b207a82745a6c9fcaa3b527d7820fc
Harunari Takata <tkt0430@keio.jp>
2024-06-03 11:58:48 +0900

    ShellScript

---
+++ runClient.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Navigate to the myRabia/rabia/client directory
+cd rabia/client
+
+# Build the client.go file
+go build
+
+# Run the compiled executable
+./client
+
---
+++ runReplica.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+# Navigate to the myRabia/rabia/replica directory
+cd rabia/replica
+
+# Build the replica.go file
+go build 
+
+# Run the compiled executable
+./replica
+
