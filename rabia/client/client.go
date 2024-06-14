dc86416da5aad139c2c0c344e4bd26a007400823
Harunari Takata <tkt0430@keio.jp>
2024-06-14 17:01:53 +0900

    Update client.go

--- rabia/client/client.go
+++ rabia/client/client.go
@@ -22,7 +22,7 @@ func main() {
 	
 	if choose == "A" {
 		for i := 0; i < commandNum; i++ {
-			var command string = generateRandomCommand(100)
+			var command string = generateRandomCommand(0)
 
 			conn, err := net.Dial("tcp", IPList[i%3]+":8080")
 			if err != nil {
