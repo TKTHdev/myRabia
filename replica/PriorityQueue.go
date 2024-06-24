5614b5f0d492a3f54fc2fa24bc6a3262f7889e99
Harunari Takata <tkt0430@keio.jp>
2024-06-24 16:12:19 +0900

    Update PriorityQueue.go

--- replica/PriorityQueue.go
+++ replica/PriorityQueue.go
@@ -1,14 +1,12 @@
 package main
 
-import(
-	"container/heap"
-)
 
 type PriorityQueue []*CommandData
 
 func (pq PriorityQueue) Len() int { return len(pq) }
 
 func (pq PriorityQueue) Less(i, j int) bool {
+	
 	return pq[i].Timestamp.Before(pq[j].Timestamp)
 }
 
@@ -19,7 +17,6 @@ func (pq PriorityQueue) Swap(i, j int) {
 func (pq *PriorityQueue) Push(x interface{}) {
 	item := x.(*CommandData)
 	*pq = append(*pq, item)
-	heap.Fix(pq, len(*pq)-1)
 }
 
 func (pq *PriorityQueue) Pop() interface{} {
