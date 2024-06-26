4ffc2178ceeeeca751bc62314bf25d00096702df
Harunari Takata <tkt0430@keio.jp>
2024-06-26 20:11:59 +0900

    Update setup.go

--- replica/setup.go
+++ replica/setup.go
@@ -13,7 +13,7 @@ func setConnectionWithOtherReplicas(IPLists []string) []net.Conn {
 
 	for _, IP := range IPLists {
 		if IP[0]!= 'N'  && IP!= ownIP{
-			fmt.Println("Connecting to: ", IP)
+			fmt.Println("Connecting to: ", IP, ownIP)
 			conn, err := net.Dial("tcp", IP+":8080")
 			if err != nil {
 				fmt.Printf("接続エラー  %d: %v\n", conn, err)
