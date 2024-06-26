0da61429f87ea74f2afe993c891f182d225a0515
Harunari Takata <tkt0430@keio.jp>
2024-06-26 15:45:23 +0900

    added GClient

---
+++ GClient/GClient.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+var StateMachine map[string]int = make(map[string]int)
+var IPList = []string{"52.62.115.28", "52.65.112.127", "52.64.108.149"}
+var replicaNum = 3
+
+func main() {
+	command , clientNum, duration := setUp()
+	var stopChannelList []chan bool = make([]chan bool, clientNum)
+	var commandNumChannelList []chan int = make([]chan int, clientNum)
+	for i := 0; i < clientNum; i++ {
+		  go YCSB(command, stopChannelList[i],commandNumChannelList[i] , i)
+	}
+	time.Sleep(time.Duration(duration) * time.Second)
+
+	//stop each of goroutines
+	for i := 0; i < clientNum; i++ {
+		stopChannelList[i] <- true
+	}
+
+	//get the number of commands executed by each client
+	var totalCommandNum int = 0
+	for i := 0; i < clientNum; i++ {
+		totalCommandNum += <-commandNumChannelList[i]
+	}
+
+	fmt.Println("Total number of commands executed: ", totalCommandNum)
+
+
+}
+
+
+func YCSB(command string, stopChannel chan bool, commandNumChannel chan int,  ID int)  {
+
+	var readRatio int	
+
+	fmt.Println("Replica? 1, 2 or 3?")
+	var replica int
+	fmt.Scanln(&replica)
+
+	if command == "A" {
+		readRatio = 50
+	} else if command == "B" {
+		readRatio = 95
+	} else if command == "C" {
+		readRatio = 100
+	}
+
+
+	var cnt int = 0
+	
+	var replicaID string = IPList[ID%replicaNum]+":8080"
+
+	for  {
+			select{
+			case <-stopChannel:
+				fmt.Println("Client stopped")
+				commandNumChannel <- cnt
+				return 
+
+			default:
+			var command string = generateRandomCommand(readRatio)
+			fmt.Println("Command: " + command)
+			conn, err := net.Dial("tcp", replicaID)
+			if err != nil {
+				fmt.Println("Dial error", err)
+				continue
+			}
+			defer conn.Close()
+			sendData(conn, Request{CommandData: CommandData{Op: command, Timestamp: time.Now(), Seq: 0, ClientAddr: conn.LocalAddr().String()}, Redirected: false, Timestamp: 0})
+			if command[0] == 'R' {
+				var data ConsensusData
+				data, err := receiveData(conn)
+				if err != nil {
+					fmt.Println("Error in receiving data")
+				}
+				response := data.Data
+				switch response := response.(type) {
+				case ResponseToClient:
+					if response.Value == -1 {
+						fmt.Println("Key not found")
+					}
+					if response.Value != -1 {
+						//fmt.Println("Read value: ", response.Value)
+					}
+				}
+			} else {
+				var data ConsensusData
+				data, err := receiveData(conn)
+				if err != nil {
+					fmt.Println("Error in receiving data")
+				}
+				response := data.Data
+				switch response := response.(type) {
+				case ResponseToClient:
+					if response.Value == 0 {
+						//fmt.Println("Write successful")
+					} else {
+						fmt.Println("Write unsuccessful")
+					}
+				}
+			}
+			cnt++
+			fmt.Println("Command ", cnt, " completed")
+		}
+	}
+}
+
+
+func setUp()(string, int , int){
+	fmt.Println("YCSB [A] or [B] or [C] ?")
+	var command string
+	fmt.Scanln(&command)
+	fmt.Println("Number of concurrent clients?")
+	var clientNum int
+	fmt.Scanln(&clientNum)
+	fmt.Println("How long to run the test for (in seconds)?")
+	var time int
+	fmt.Scanln(&time)
+	return command, clientNum, time
+}
---
+++ GClient/header.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"math/rand"
+	"net"
+	"time"
+)
+
+type Data interface{}
+
+type ConsensusData struct {
+	Data Data
+}
+
+type CommandData struct {
+	Op          string
+	Timestamp   time.Time
+	Seq         int
+	ClientAddr  string
+	ReplicaAddr string
+}
+
+type Request struct {
+	CommandData CommandData
+	Redirected  bool
+	Timestamp   int
+}
+
+type CommandTimestamp struct {
+	Command   string
+	Timestamp int
+}
+
+type ResponseToClient struct {
+	Value int
+}
+
+func init() {
+	gob.Register(Request{})
+	gob.Register(ResponseToClient{})
+	gob.Register(ConsensusData{})
+	gob.Register(CommandData{})
+
+}
+
+func receiveData(conn net.Conn) (ConsensusData, error) {
+	var data ConsensusData
+	decoder := gob.NewDecoder(conn)
+	err := decoder.Decode(&data)
+	if err != nil {
+		fmt.Println("データ受信エラー:", err)
+		return ConsensusData{}, err
+	}
+	//fmt.Println("データ受信完了")
+	return data, nil
+}
+
+func sendData(conn net.Conn, data Data) {
+	encoder := gob.NewEncoder(conn)
+	err := encoder.Encode(ConsensusData{Data: data})
+	if err != nil {
+		fmt.Println("データ送信エラー:", err)
+		return
+	}
+}
+
+func generateRandomCommand(readRatio int) string {
+	variables := []string{"x", "y", "z", "a", "b", "c"}
+	operators := []string{"=", "+", "-", "*", "%"}
+
+	//generate read command with readRatio
+	if rand.Intn(100) < readRatio {
+		variable := variables[rand.Intn(len(variables))]
+		return fmt.Sprintf("R %s", variable)
+	} else {
+		variable := variables[rand.Intn(len(variables))]
+		operator := operators[rand.Intn(len(operators))]
+		value := rand.Intn(100)
+		return fmt.Sprintf("%s %s %d", variable, operator, value)
+	}
+}
