e162853be5f8d9700525ca5642af0f651b22a531
Harunari Takata <tkt0430@keio.jp>
2024-06-30 17:43:13 +0900

    Update GClient.go

--- GClient/GClient.go
+++ GClient/GClient.go
@@ -7,7 +7,7 @@ import (
 )
 
 var StateMachine map[string]int = make(map[string]int)
-var IPList = []string{"", "52.65.112.127", "52.62.115.28"}
+var IPList = []string{"52.64.108.149", "52.65.112.127", "52.62.115.28"}
 var replicaNum = 3
 
 func main() {
